d2257ca8d44b8f718b88f8f0ca81745bcf8733cb
cnpapado <14839869+cnpapado@users.noreply.github.com>
2020-03-06 18:15:09 +0200

    wrong draft

--- draft.cpp
+++ draft.cpp
@@ -1,12 +1,25 @@
 class Bitboard{
-	protected:
+	public:
+
+	protected: 
 		unsigned long long int b;
 };
-
+class Notation{
+	Notation(); //constructor that accepts valid notations
+	Notation(unsigned long long int bin); //second constructor
+	int getNotationIndex(); //return index
+	protected:
+		int rank;
+		char file;
+		int notationIndex;
+};
+//class Move{};
 class Chessboard{
 	public:
+		void reset();// make object equal to the starting state
+		play()
 	protected:
-		Bitboard 	whitePawns,
+		Bitboard	whitePawns,
 					blackPawns,
 					whiteRooks,
 					blackRooks,
@@ -19,4 +32,18 @@ class Chessboard{
 					whiteKing,
 					blackKing;
 		bool sideToMove;			
+}
+class Game{
+
+};
+
+int main(){
+	Chessboard test;
+	test.reset();
+
+	Notation currentMove;
+	while(!Game.finished()){
+
+	}
+	return 0;
 }
